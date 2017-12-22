2cffcfc8df940dbcc4bf708d36f77dc8227dbbee
kephamose <kepham@gmail.com>
2017-12-21 22:47:33 -0600

    Chapter 29

--- Users/Startup.cs
+++ Users/Startup.cs
@@ -58,6 +58,8 @@ namespace Users
             app.UseStaticFiles();
             app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
+            AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices,
+                Configuration).Wait();
         }
     }
 }
